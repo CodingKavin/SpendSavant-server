8ef29faab9d9cec4f871152431da2afb307ce89f
Kavin Paul <kavinp622@gmail.com>
2026-02-17 10:47:53 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import users from "./routes/users.js";
+import expenses from "./routes/expenses.js";
+
+
+const port = process.env.PORT;
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // allows parsing JSON data from req objects
+
+app.use("/users", users);
+app.use("/expenses", expenses);
+
+app.listen(port, () => console.log(`Listening on ${port}`));
