6d39dd0d82ea9025568888e590c78cae7985522c
Kavin Paul <kavinp622@gmail.com>
2026-02-20 10:32:00 -0700

    set up middleware using jose library for JWT token verification from supabase

--- routes/expenses.js
+++ routes/expenses.js
@@ -1,56 +1,20 @@
 import express from "express";
-import connection from "../utils/mysql.js";
-import { v4 as uuidv4 } from 'uuid';
+import sql from "../utils/postgres.js";
+import { authenticateJWT } from "./utils/authMiddleware.js"
 
 const router = express.Router();
+
+router.use(authenticateJWT)
 
 router.route("/").get(async (req, res) => {
     const sql = "";
     try {
-        const [results] = await connection.query(sql);
+        const expenses = await sql`SELECT * FROM expenses WHERE user_id = ${req.user.id}`;
         res.json(results);
     } catch (error) {
         console.log(error);
         res.status(500).send("Error Occured on server");
     }
 })
-
-router.route("/:id")
-    .get(async (req, res) => {
-        const expenseId = req.params.id;
-        const sql = ``;
-        try {
-            const [results] = await connection.query(sql, [expenseId]);
-            if (results.length === 0) {
-                return res.status(404).json({
-                    message: `No expense found with id ${expenseId}`
-                });
-            }
-            res.json(results[0]);
-        } catch (error) {
-            console.log(error);
-            res.status(500).send("Error occured on the server");
-        }
-    })
-
-    .delete(async (req, res) => {
-        const expenseId = req.params.id;
-
-        try {
-            const [result] = await connection.query(
-                "DELETE FROM expenses WHERE id = ?",
-                [expenseId]
-            );
-            if (result.affectedRows === 0) {
-                return res.status(404).json({
-                    message: `Expense with ID ${expenseId} not found`
-                });
-            }
-            res.sendStatus(204);
-        } catch (error) {
-            console.log(error);
-            res.status(500).send("Error occured on the server");
-        }
-    });
 
 export default router;
