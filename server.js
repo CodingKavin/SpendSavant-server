13190d515e7edf10d017ea16a19c0a9ffdbbd3c5
Kavin Paul <kavinp622@gmail.com>
2026-02-27 00:15:45 -0700

    add budgets route, validation and get and post requests

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import expenses from "./routes/expenses.js";
+import budgets from "./routes/budgets.js";
 
 
 const port = process.env.PORT || 5000;
@@ -11,5 +12,6 @@ app.use(cors());
 app.use(express.json()); // allows parsing JSON data from req objects
 
 app.use("/expenses", expenses);
+app.use("/budgets", budgets);
 
 app.listen(port, () => console.log(`Listening on ${port}`));
--- utils/expenseValidation.js
+++ utils/expenseValidation.js
@@ -8,6 +8,13 @@ const expenseSchema = z.object({
     recurrence: z.enum(["daily", "weekly", "monthly"]).optional(),
 });
 
+const budgetSchema = z.object({
+    user_id: z.string().min(1),
+    month: z.number.int().min(1).max(12),
+    year: z.number().int().min(2000),
+    amount: z.number().nonnegative()
+})
+
 const expenseUpdateSchema = expenseSchema.partial();
 
 export function validateExpense(data) {
@@ -20,6 +27,12 @@ export function validateExpense(data) {
 
 export function validateExpenseUpdate(data) {
     const parsed = expenseUpdateSchema.safeParse(data);
+    if (!parsed.success) return parsed.error.errors;
+    return null;
+}
+
+export function validateBudget(data) {
+    const parsed = budgetSchema.safeParse(data);
     if (!parsed.success) return parsed.error.errors;
     return null;
 }
